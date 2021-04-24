616e3f4c51ae64a8cede05d3cdb9c27bfba7eb10
Huamies <1120830301@qq.com>
2021-04-25 00:36:08 +0800

    1.增加了主页（引入插件） 2.调整了登入页的请求

---
+++ pages/static/index.vue
@@ -0,0 +1,127 @@
+<template>
+	<view>
+		<view class="title" :class="topNavStyle.class" :style="topNavStyle.style">
+			<view class="flex_col">
+				<view class="box1"></view>
+				<view class="flex_grow flex_col flex_center tab">
+					<view v-for="(item,index) in topNavArr" :key="index"
+					 :class="{ 'active':topNavIndex==index }" 
+					 :data-index="index" @tap="changeTopNav">{{item}}</view>
+				</view>
+				<view class="box1 align_r">
+					
+				</view>
+			</view>
+		</view>
+		<card-swiper></card-swiper>
+		<view style="height: 600px;">
+			<view style="padding-top: 50rpx;text-align: center;">向上滚动查看标题栏渐变效果</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import cardSwiper from "@/pages/component/helang-cardSwiper/helang-cardSwiper.vue"
+	export default {
+		data() {
+			return {
+				topNavIndex:0,
+				topNavArr:['推荐','热门','新作'],
+				pageScrollTop:0,	// 页面滚动距离
+			}
+		},
+		components:{
+			cardSwiper
+		},
+		computed:{
+			topNavStyle(){
+				let r = this.pageScrollTop  / 100;
+				return {
+					"class":r>=0.85?'style2':'',
+					"style":`background-color: rgba(66, 185, 131,${r>=1?1:r});`
+				}
+			}
+		},
+		onLoad() {
+			
+		},
+		// 页面滚动监听
+		onPageScroll(e){
+			this.pageScrollTop = Math.floor(e.scrollTop);
+		},
+		methods: {
+			// 顶部导航改变 
+			changeTopNav(e){
+				this.topNavIndex = e.currentTarget.dataset.index
+			},
+			// 去搜索
+			toSearch(){
+				uni.navigateTo({
+					url:`/pages/search/search`  
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "lib/global.scss";
+	
+	page{
+		background-color: #fff;
+	}
+	/* 标题栏 */
+	.title{
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: auto;
+		padding-top: var(--status-bar-height);
+		z-index: 10;
+		background-color: rgba(66,185,131,0);
+		color: rgba(255,255,255,0.8);
+		
+		&>view{
+			height: 44px;
+		}
+		
+		.box1{
+			width: 60rpx;
+			margin: 0 40rpx;
+			font-size: 36rpx;
+		}
+		
+		
+		.tab{
+			&>view{
+				margin: 0 30rpx;
+				line-height: 64rpx;
+				font-size: 36rpx;
+				position: relative;
+				letter-spacing: 0;
+				transition: transform 0.3s ease-in-out 0s;
+				transform: scale(1,1);
+				
+				&.active{
+					color: rgba(255,255,255,1);
+					transform: scale(1.15,1.15);
+				}
+			}
+		}
+		
+		&.style2{
+			color: #FFF;
+			background-color: rgba(66, 185, 131,1);
+			
+			.tab{
+				&>view{
+					&.active{
+						color: #FFF;
+					}
+				}
+			}
+		}
+	}
+
+</style>
--- pages/static/login.vue
+++ pages/static/login.vue
@@ -16,50 +16,25 @@
 				
 				
 				<form>
-					
-					<view class="cu-form-group">
-						<view class="title">输入框</view>
-						<input placeholder="请输入用户名" v-model="inName" ></input>
-					</view>
-					
-					<view class="cu-form-group">
-						<view class="title">输入框</view>
-						<input placeholder="请输入密码" v-model="inPwd" ></input>
-					</view>
-					
-					<view class="cu-form-group margin-top">
-						<view class="title">普通选择</view>
-						<picker @change="PickerChange" :value="index" :range="picker">
-							<view class="picker">
-								{{index>-1?picker[index]:'请选择'}}
-							</view>
-						</picker>
-					</view>
-					
-					<radio-group class="block" @change="RadioChange">
-						<view class="cu-form-group margin-top">
-							<view class="title">单选操作(radio)</view>
-							<radio :class="radio=='A'?'checked':''" :checked="radio=='A'?true:false" value="A"></radio>
-						</view>
-						<!-- #ifndef MP-ALIPAY -->
-						<view class="cu-form-group">
-							<view class="title">定义样式</view>
-							<radio class='radio' :class="radio=='B'?'checked':''" :checked="radio=='B'?true:false" value="B"></radio>
-						</view>
-						<view class="cu-form-group">
-							<view class="title">定义颜色</view>
-							<view>
-								<radio class='blue radio' :class="radio=='C'?'checked':''" :checked="radio=='C'?true:false" value="C">男</radio>
-								<radio class='blue radio' :class="radio=='D'?'checked':''" :checked="radio=='D'?true:false" value="D">女</radio>
-							</view>
-						</view>
-						<!-- #endif -->
-					</radio-group>
-				
 					<view class="padding flex flex-direction">
-						<button class="cu-btn bg-red margin-tb-sm lg" @click="OnLogin()">嫣红</button>
-					</view>
+					
+						<view class="cu-form-group" style="margin-bottom: 5%;">
+							<view class="title">账	户：</view>
+							<input placeholder="请输入用户名" v-model="inName" ></input>
+						</view>
+						
+						<view class="cu-form-group">
+							<view class="title">密	码：</view>
+							<input placeholder="请输入密码" v-model="inPwd" ></input>
+						</view>
+						
+						<view class="padding flex flex-direction">
+							<button class="cu-btn bg-blue lg" @click="OnLogin()">登入</button>
+							<button class="cu-btn bg-red margin-tb-sm lg">取消</button>
+						</view>
 				
+				
+					</view>
 				</form>
 				
 			</view>
@@ -83,25 +58,56 @@
         },
         methods: {
             OnLogin(){
-				uni.request({
-					url:'http://localhost:80/queryUser',
-					methods:'GET',
-					dataType :'JSON',
-					async: false,
-					data: {
-						userName: 'admin',
-						userPwd:'123456'
-					},
-					success: res => {
-						debugger
-							console.log('返回', res.data);
-					},
-					fail:res =>{
-						debugger
-						//失败时候回调
-					}
+				
+				//不可信任信息筛选
+				if(this.inName == "" && this.inPwd == ""){
 					
-				})
+					uni.showToast({
+						title: '输入项请勿留空',
+						icon:'none',
+						duration: 1000
+					});
+					
+				}else{
+				
+					uni.request({
+						url:'http://localhost:80/queryUser',
+						methods:'GET',
+						dataType :'JSON',
+						async: false,
+						data: {
+							userName: this.inName,
+							userPwd: this.inPwd
+						},
+						success: res => {
+							debugger
+							if(res.data.length < 1){
+								uni.showToast({
+									title: '账户或者密码错误，请重试！',
+									duration: 1000
+								});
+							}else{
+								console.log('返回', res.data);
+								uni.reLaunch({
+									url:'index'
+								})
+							}
+							
+							
+								
+						},
+						fail:res =>{
+							debugger
+							//失败时候回调
+							uni.showToast({
+								title: '系统错误，请联系管理员',
+								icon:'none',
+								duration: 1000
+							});
+						}
+						
+					})
+				}
 				
 			},
 			PickerChange(e) {
